1ed59314e085cb15c50bb0c3dd639549864567f9
elrish <ellerish11@gmail.com>
2018-05-30 11:40:42 +0200

    fix

--- src/providers/cart-service/cart-service.ts
+++ src/providers/cart-service/cart-service.ts
@@ -10,8 +10,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CartServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello CartServiceProvider Provider');
+  constructor() {
+
   }
 
 }
